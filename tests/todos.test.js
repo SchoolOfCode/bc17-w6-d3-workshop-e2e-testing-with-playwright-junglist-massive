10db93346fa94a1e7a08d672b74d6600994fce99
Jocelyne Jones <gothling@jocely.net>
2024-08-08 12:06:33 +0100

    test for heading

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -1,7 +1,16 @@
-import{test,expect} from '@playwright/test'
+import{test,expect} from '@playwright/test';
+
 test ("practice test should pass",async ({page}) =>{
     await page.goto("http://localhost:3000");
-}
 
+    const textbox = page.getByRole('textbox');
+    await textbox.fill('some value');
 
-)
+    await expect(textbox).toHaveValue('some value');
+})
+
+test ("heading appears",async ({page}) =>{
+    await page.goto("http://localhost:3000");
+
+    await expect(page.getByText('Todo List App')).toBeVisible();
+})
